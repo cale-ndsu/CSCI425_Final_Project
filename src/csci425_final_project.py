2c585d728d5ae865419104ad4293813bf36cab76
Maison Kasprick <maison.kasprick@gmail.com>
2025-04-20 16:02:59 -0500

    Changed the model from linear regression to random forest
    We found that linear regression will not work for our dataset, so we found that random forest will work for our dataset.

--- src/csci425_final_project.py
+++ src/csci425_final_project.py
@@ -10,14 +10,14 @@ The main file to be executed for the project.
 from preprocessing import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 
 
 def standardization(data:list) -> list:
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
     # print(f'{X} {y}') # Use to check data separation
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
     # Standarize X_train and X_test
     sc = StandardScaler()
@@ -25,20 +25,20 @@ def standardization(data:list) -> list:
     X_train_std = sc.transform(X_train)
     X_test_std = sc.transform(X_test)
 
+    # Saving all x and y variables to output
     variables = []
     variables.append(X_train_std)
     variables.append(X_test_std)
     variables.append(y_train)
     variables.append(y_test)
-
     return variables
 
 
-def regression_results(X_train, X_test, y_train, y_test):
+def random_forest_results(X_train:list, X_test:list, y_train:list, y_test:list) -> None:
     # print(f'{X_train} {X_test} {y_train} {y_test}') # Use to check match with cold and warm variables
-    linear = LinearRegression().fit(X_train, y_train)
-    print('Linear Regression Training Accuracy: %.3f' % linear.score(X_train, y_train))
-    print('Linear Regression Testing Accuracy: %.3f' % linear.score(X_test, y_test))
+    forest = RandomForestClassifier().fit(X_train, y_train)
+    print('Random Forest Training Accuracy: %.3f' % forest.score(X_train, y_train))
+    print('Random Forest Testing Accuracy: %.3f' % forest.score(X_test, y_test))
 
 
 def main():
@@ -51,20 +51,12 @@ def main():
     cold_variables = standardization(data_cold)
     warm_variables = standardization(data_warm)
     # print(f'{cold_variables} {warm_variables}') # Use to check to make sure all variables are there
-
-    X_train_cold = cold_variables[0]
-    X_test_cold = cold_variables[1]
-    y_train_cold = cold_variables[2]
-    y_test_cold = cold_variables[3]
-    
-    X_train_warm = warm_variables[0]
-    X_test_warm = warm_variables[1]
-    y_train_warm = warm_variables[2]
-    y_test_warm = warm_variables[3]
+    X_train_cold, X_test_cold, y_train_cold, y_test_cold = cold_variables
+    X_train_warm, X_test_warm, y_train_warm, y_test_warm = warm_variables
 
     # Pass both cold and warm x and y through linear regression
-    regression_results(X_train_cold, X_test_cold, y_train_cold, y_test_cold)
-    regression_results(X_train_warm, X_test_warm, y_train_warm, y_test_warm)
+    random_forest_results(X_train_cold, X_test_cold, y_train_cold, y_test_cold)
+    random_forest_results(X_train_warm, X_test_warm, y_train_warm, y_test_warm)
 
 
 if __name__ == '__main__':
