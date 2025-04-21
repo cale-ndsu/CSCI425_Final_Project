38e3a59b204f72577b0676b545bdffdf249ef205
Maison Kasprick <maison.kasprick@gmail.com>
2025-04-20 20:53:08 -0500

    Removed the no longer needed print statements and added some comments
    Removed the print statements that are no longer needed. I also added some comments to sections of the code.

--- src/csci425_final_project.py
+++ src/csci425_final_project.py
@@ -15,9 +15,9 @@ import numpy as np
 
 
 def standardization(data:list) -> list:
+    # Split the data into x and y then split again to train and test variables
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
-    # print(f'{X} {y}') # Use to check data separation
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
     # Standarize X_train and X_test
@@ -36,26 +36,30 @@ def standardization(data:list) -> list:
 
 
 def random_forest_results(X_train:list, X_test:list, y_train:list, y_test:list, dataset:str) -> None:
-    # print(f'{X_train} {X_test} {y_train} {y_test}') # Use to check match with cold and warm variables
+    # Create the forest model and print the accuracies of both train and test variables
     print(f'{dataset} Dataset')
     print(f'------------')
     forest = RandomForestClassifier().fit(X_train, y_train)
     print('Training Accuracy: %.3f' % forest.score(X_train, y_train))
     print('Testing Accuracy: %.3f' % forest.score(X_test, y_test))
+
+    # Create test temperatures to run through the model
     test_temperatures = None
     if (dataset == "Cold"):
         test_temperatures = np.arange(-30, 33).reshape(-1,1)
     else:
         test_temperatures = np.arange(33, 120).reshape(-1,1)
     
+    # Run the test temperatures through the model and display the average of each severity level 
     probabilities = forest.predict_proba(test_temperatures)
     avgs = calculate_average_probabilities(probabilities)
     i = 0
     for avg in avgs:
-        print(f'Average Probability of Traffic Disruption with Severity Level of {i+1}: {avg}')
+        print(f'Average Probability of Traffic Disruption with Severity Level of {i+1}: {avg: .5f}')
         i += 1
     print()
     
+
 def calculate_average_probabilities(probabilities: np.ndarray) -> list:
     j = 0
     i = 0
@@ -73,20 +77,18 @@ def calculate_average_probabilities(probabilities: np.ndarray) -> list:
         
 
 def main():
+    # Preprocess the US accidents dataset
     np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format})
     data = preprocessing()
-    # print(data) # Use to check data preprocessing
-    data_cold = data[0]
-    data_warm = data[1]
+    data_cold, data_warm = data
 
     # Standardize both the cold and warm datasets
     cold_variables = standardization(data_cold)
     warm_variables = standardization(data_warm)
-    # print(f'{cold_variables} {warm_variables}') # Use to check to make sure all variables are there
     X_train_cold, X_test_cold, y_train_cold, y_test_cold = cold_variables
     X_train_warm, X_test_warm, y_train_warm, y_test_warm = warm_variables
 
-    # Pass both cold and warm x and y through linear regression
+    # Pass both cold and warm x and y through a random forest model
     random_forest_results(X_train_cold, X_test_cold, y_train_cold, y_test_cold, "Cold")
     random_forest_results(X_train_warm, X_test_warm, y_train_warm, y_test_warm, "Warm")
 
